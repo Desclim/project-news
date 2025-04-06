f77a6a303ec46e8c2e3a3ac1a74aec40456859e0
Desclim <declownmail@gmail.com>
2025-04-06 16:51:56 +0300

    File structure

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,17 @@
 import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
+import path from "path";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {
+            app: path.resolve(options.paths.src, 'app'),
+            Pages: path.resolve(options.paths.src, 'pages'),
+            helpers: path.resolve(options.paths.src, 'helpers'),
+        },
     }
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from 'react-router-dom'
-import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "Pages/AboutPage";
+import {MainPage} from "Pages/MainPage";
 const App = () => {
 const { theme, toggleTheme } = useTheme()
-    const bool = true
     return (
         <div className={classNames('app',{},[theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
@@ -15,8 +14,8 @@ const { theme, toggleTheme } = useTheme()
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
